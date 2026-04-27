d2e12ebb0c9aa0bd35645be1a8b2a8f2e1c1cf29
Ruthikakanapuram <ruthika5050@gmail.com>
2026-04-27 11:41:12 +0530

    added environment objects and action library

---
+++ action_registry.py
@@ -0,0 +1,8 @@
+LIBRARY_ACTIONS = [
+    "get_position(object_id)",
+    "move_to_grasp_pos(position)",
+    "close_gripper()",
+    "move_to_release_pos(position)",
+    "open_gripper()",
+    "move_to_temp_pos(temp_slot)",
+]
---
+++ environment.py
@@ -0,0 +1,8 @@
+ENVIRONMENT_OBJECTS = {
+    "red_cube_1": {"type": "cube", "color": "red", "position": "A2"},
+    "blue_box_1": {"type": "box", "color": "blue", "position": "B4"},
+    "green_cyl_1": {"type": "cylinder", "color": "green", "position": "C1"},
+    "yellow_tray_1": {"type": "tray", "color": "yellow", "position": "D3"},
+    "storage_bin_1": {"type": "bin", "color": "gray", "position": "E5"},
+    "purple_cube_1": {"type": "cube", "color": "purple", "position": "A5"},
+}
